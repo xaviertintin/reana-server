94c168d758f69a23cbe312c5724f2db4cdd146c8
alextintin007 <alextintin007@outlook.com>
2024-01-09 22:22:11 -0500

    Test3

--- reana_server/rest/workflows.py
+++ reana_server/rest/workflows.py
@@ -336,6 +336,7 @@ def get_workflows(user, **kwargs):  # noqa
                 "message": "Something went wrong."
               }
     """
+    import wdb; wdb.set_trace()
     try:
         type_ = request.args.get("type", "batch")
         search = request.args.get("search")
